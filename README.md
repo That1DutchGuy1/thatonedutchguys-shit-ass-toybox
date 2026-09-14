9eb6dad824eb77386ac77f4c81e5efbd67da0bae
That1DutchGuy1 <dwildeboer74@gmail.com>
2026-09-14 23:15:48 +0200

    The Michael Rosen Update & Rick Astley sprite fix

--- README.md
+++ README.md
@@ -103,7 +103,8 @@
 <p><strong><font size="2">Grand Meme Prix 3D got a small lil update!</font></strong></p>  <br>
 
 * CD-i Ganon has been added as a new driver! He will make your face the greatest in Koridai if you win as him! 🐷
-* Unique voice lines for Ganon, of course! 🎙️
+* Michael Rosen has been added as a new driver! He loves hot food just as much as winning! 🇬🇧
+* Unique voice lines for Ganon and Michael Rosen, of course! 🎙️
 * Local multiplayer co-op mode can now fucking feature up to two CPU drivers to spice up the race with your friend... that is if you actually have any friends! 🙃
 * An actual fucking grass texture on the grass is now in the game! 🌱
 * A lot of fucking trees are now scattered outside the stadium! 🌲
--- grand-meme-prix/grand_meme_prix.js
+++ grand-meme-prix/grand_meme_prix.js
@@ -29,6 +29,7 @@ const CHARACTERS = [
   { name: "Malleo",        img: "assets/Malleo.png",         color: 0xb30c0c, lightbarColor: 0xFF0000, voicelines: ['assets/voicelines/malleo-hit.mp3', 'assets/voicelines/malleo-win.mp3', 'assets/voicelines/malleo-lose.mp3'] }, // red
   { name: "Mayor Cravendish", img: "assets/Mayor-Cravendish.png", color: 0x0fa4c8, lightbarColor: 0x0000FF, voicelines: ['assets/voicelines/mayor-cravendish-hit.mp3', 'assets/voicelines/mayor-cravendish-win.mp3', 'assets/voicelines/mayor-cravendish-lose.mp3'] }, // blue
   { name: "Ganon",          img: "assets/Ganon.png",           color: 0x3C0008, lightbarColor: 0x5D100A, voicelines: ['assets/voicelines/ganon-hit.mp3', 'assets/voicelines/ganon-win.mp3', 'assets/voicelines/ganon-lose.mp3'] }, // maroon
+  { name: "Michael Rosen",     img: "assets/Michael-Rosen.png",    color: 0x708090, lightbarColor: 0xC0C0C0, voicelines: ['assets/voicelines/michael-rosen-hit.mp3', 'assets/voicelines/michael-rosen-win.mp3', 'assets/voicelines/michael-rosen-lose.mp3'] }, // grey
 ];
 
 // =============================================
